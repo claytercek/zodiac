269c7cb3e65ff4604689442eae30198c9835b858
Clay Tercek <clay.tercek@gmail.com>
2018-02-21 16:48:19 -0500

    made mobile experience less jarring

--- main.js
+++ main.js
@@ -2,18 +2,16 @@
 	var artistIconList = document.querySelectorAll(".artistIcon");
 
 	for (const icon of artistIconList) {
-		(function() {
-			var name = icon.getAttribute("alt");
-			var nameNoSpaces = name.replace(/ /g, "");
-			icon.addEventListener(
-				"click",
-				function() {
-					playAudio(nameNoSpaces);
-					swapDiv(nameNoSpaces);
-				},
-				false
-			);
-		})();
+		let name = icon.getAttribute("alt");
+		let nameNoSpaces = name.replace(/ /g, "");
+		icon.addEventListener(
+			"click",
+			function() {
+				playAudio(nameNoSpaces);
+				swapDiv(nameNoSpaces);
+			},
+			false
+		);
 	}
 
 	var audio = new Audio(); //establish audio variable
@@ -30,19 +28,16 @@
 			if (oldWrapper !== "") {
 				oldWrapper.style.animationDelay = "0s";
 				oldWrapper.classList.remove("slideinAnimation");
-				newWrapper.style.animationDelay = "0.6s";
+				if (window.innerWidth > 720) {
+					newWrapper.style.animationDelay = "0.6s";
+				} else {
+					newWrapper.style.animationDelay = "1.5s";
+				}
 				oldWrapper.classList.add("wipeOutAnimation");
 			}
 			newWrapper.classList.remove("wipeOutAnimation");
 			newWrapper.classList.add("slideinAnimation");
 			oldWrapper = newWrapper;
 		}
-		if (window.innerWidth < 720) {
-			moveIconGrid(artistName);
-		}
-	}
-	grid = document.querySelector("aside");
-	function moveIconGrid() {
-		return;
 	}
 }
