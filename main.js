56e70221d9e370fdd1f7b969f4e964404a7c8512
Clay Tercek <clay.tercek@gmail.com>
2018-02-22 14:48:30 -0500

    ✨Implemented zodiac algorithm

--- main.js
+++ main.js
@@ -14,6 +14,61 @@
 		);
 	}
 
+	var form = document.querySelector("form");
+	var warning = document.querySelector(".warning");
+	function submitForm() {
+		var fname = form.elements[0].value;
+		var lname = form.elements[1].value;
+		var date = form.elements[2].value;
+		console.log(date);
+		date = date.split("-");
+
+		whichDayOfMonth = date[2];
+		whichMonth = date[1];
+
+		if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
+			AstroSign = "Cap";
+		} else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+			AstroSign = "Sag";
+		} else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
+			AstroSign = "Sco";
+		} else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+			AstroSign = "Lib";
+		} else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+			AstroSign = "Vir";
+		} else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+			AstroSign = "Leo";
+		} else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+			AstroSign = "Can";
+		} else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+			AstroSign = "Gem";
+		} else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+			AstroSign = "Tau";
+		} else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+			AstroSign = "Ari";
+		} else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+			AstroSign = "Pis";
+		} else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+			AstroSign = "Aqu";
+		}
+		var dictionary = {
+			Cap: "GerryMulligan",
+			Sag: "TheloniousMonk",
+			Sco: "CharlieParker",
+			Lib: "MilesDavis",
+			Vir: "DukeEllington",
+			Leo: "JohnColtrane",
+			Can: "BillEvans",
+			Gem: "ChetBaker",
+			Tau: "CannonballAdderley",
+			Ari: "LouisArmstrong",
+			Pis: "CountBasie",
+			Aqu: "BillieHoliday"
+		};
+		playAudio(dictionary[AstroSign]);
+		swapDiv(dictionary[AstroSign]);
+	}
+
 	var audio = new Audio(); //establish audio variable
 	function playAudio(artistName) {
 		audio.pause(); //incase audio is already playing
